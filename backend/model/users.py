c6c2877e0da308efacc0762214834aec00f314c5
tuyetdt <tuyetdt96.act@gmail.com>
2018-08-27 10:44:04 +0700

    test

---
+++ backend/model/users.py
@@ -0,0 +1,85 @@
+from model.connect_db import DatabaseDriver
+
+
+class Users:
+    @staticmethod
+    def confirm_account(mail, user_password):
+        try:
+            query = """select * from users
+                        where mail = ? and user_password = ?"""
+            args = [mail, user_password]
+            result = DatabaseDriver().query_db(query, args, one=True)
+        except Exception as e:
+            print(e)
+            return False
+        return result
+
+    @staticmethod
+    def get_users():
+        try:
+            result = DatabaseDriver().query_db("""select * from users""")
+        except Exception as e:
+            print(e)
+            return False
+        return result
+
+    @staticmethod
+    def find_mail(mail):
+        try:
+            query = """select mail from users where mail = ?"""
+            args = [mail]
+            result = DatabaseDriver().query_db(query, args, one=True)
+            if result is not None:
+                return True
+        except Exception as e:
+            return False
+        return False
+
+    @staticmethod
+    def get_user(user_id):
+        try:
+            query = """select * from users where user_id = ?"""
+            args = [user_id]
+            result = DatabaseDriver().query_db(query, args, one=True)
+        except Exception as e:
+            print(e)
+            return False
+        return result
+
+    @staticmethod
+    def create_user(mail, user_password, name, position):
+        try:
+            driver = DatabaseDriver()
+            args = [mail, user_password, name, position]
+            new_id = driver.exec_command("""insert into users(mail, user_password, user_name, position)
+                                            values (?, ?, ?, ?, ?)""", args)
+            return new_id
+        except Exception as e:
+            print(e)
+            return False
+
+    @staticmethod
+    def edit_user(user_id, mail, user_password, user_name):
+        try:
+            driver = DatabaseDriver()
+            args = [user_id, mail, user_password, user_name]
+            driver.exec_command("""update users set 
+                                    mail=?, 
+                                    user_password=?, 
+                                    user_name=?
+                                    where user_id=?""", args)
+        except Exception as e:
+            print(e)
+            return False
+        return True
+
+    @staticmethod
+    def del_user(user_id):
+        try:
+            driver = DatabaseDriver()
+            args = [user_id]
+            driver.exec_command("""delete users where user_id=?""", args)
+        except Exception as e:
+            print(e)
+            return False
+        return True
